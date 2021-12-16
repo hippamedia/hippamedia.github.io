05c08d67ed831ea397d82bb2ec4e3b35f65dd307
hippamedia <j.hyytiainen@gmail.com>
2021-12-16 20:12:36 +0200

    animation2

--- js.js
+++ js.js
@@ -1,15 +1,103 @@
-var container = document.getElementById('container');
-var windowHeight = window.innerHeight;
-var windowWidth = window.innerWidth;
-var scrollArea = 1000 - windowHeight;
-var square1 = document.getElementsByClassName('square')[0];
-var square2 = document.getElementsByClassName('square')[1];
+var _containerHeight = 4000;
+var _width, _height, _scrollHeight;
+var letters = document.getElementsByTagName('span');
+var _movingElements = [];
+var _scrollPercent = 0;
+var pre = prefix();
+var _jsPrefix  = pre.lowercase;
+if(_jsPrefix == 'moz') _jsPrefix = 'Moz'
+var _cssPrefix = pre.css;
+var _positions = [
+  {
+    name: 'vihmo', 
+   	start: {
+    	percent: 0.0, x: 0.0, y: 0.08
+  	},
+    end: {
+      percent: 0.5, x: 0.0, y: 0.28
+    }
+  },
+  {
+    name: 'media', 
+   	start: {
+    	percent: 0.0, x: 0.0, y: 0.13
+  	},
+    end: {
+      percent: 0.5, x: 1, y: 0.13
+    }
+  }
+]
 
-// update position of square 1 and square 2 when scroll event fires.
-window.addEventListener('scroll', function() {
-  var scrollTop = window.pageYOffset || window.scrollTop;
-  var scrollPercent = scrollTop/scrollArea || 0;
+resize();
+initMovingElements();
+
+function initMovingElements() {
+  for (var i = 0; i < _positions.length; i++) {
+    _positions[i].diff = {
+      percent: _positions[i].end.percent - _positions[i].start.percent,
+      x: _positions[i].end.x - _positions[i].start.x,
+      y: _positions[i].end.y - _positions[i].start.y,
+    }
+    var el = document.getElementsByClassName('logo '+_positions[i].name)[0];
+    _movingElements.push(el);
+  }
+}
+
+function resize() {
+	_width = window.innerWidth;
+  _height = window.innerHeight;
+  _scrollHeight = _containerHeight-_height;
+}
+
+function rotateLetters() {
+  for (var i = 0; i < letters.length; i++) {
+    letters[i].style[_jsPrefix+'Transform'] = 'rotateY('+(_scrollPercent*500)+'deg)'
+  }
+}
+
+function updateElements() {
+  for (var i = 0; i < _movingElements.length; i++) {
+    var p = _positions[i];
+    if(_scrollPercent <= p.start.percent) {
+      _movingElements[i].style[_jsPrefix+'Transform'] = 'translate3d('+(p.start.x*_width)+'px, '+(p.start.y*_containerHeight)+'px, 0px)';
+    } else if(_scrollPercent >= p.end.percent) {
+      _movingElements[i].style[_jsPrefix+'Transform'] = 'translate3d('+(p.end.x*_width)+'px, '+(p.end.y*_containerHeight)+'px, 0px)';
+    } else {
+      _movingElements[i].style[_jsPrefix+'Transform'] = 'translate3d('+(p.start.x*_width + (p.diff.x*(_scrollPercent-p.start.percent)/p.diff.percent*_width))+'px, '+
+        (p.start.y*_containerHeight + (p.diff.y*(_scrollPercent-p.start.percent)/p.diff.percent*_containerHeight))+'px, 0px)';
+    }
+  }
+}
+
+
+
+function loop() {
+  _scrollOffset = window.pageYOffset || window.scrollTop;
+  _scrollPercent = _scrollOffset/_scrollHeight || 0;
+  rotateLetters();
+  updateElements();
   
-  square1.style.left = scrollPercent*window.innerWidth + 'px';
-  square2.style.left = 400 - scrollPercent*window.innerWidth*0.6 + 'px';
-});
+  requestAnimationFrame(loop);
+}
+
+loop();
+
+window.addEventListener('resize', resize);
+
+/* prefix detection http://davidwalsh.name/vendor-prefix */
+
+function prefix() {
+  var styles = window.getComputedStyle(document.documentElement, ''),
+    pre = (Array.prototype.slice
+      .call(styles)
+      .join('') 
+      .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+    )[1],
+    dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+  return {
+    dom: dom,
+    lowercase: pre,
+    css: '-' + pre + '-',
+    js: pre[0].toUpperCase() + pre.substr(1)
+  };
+}
