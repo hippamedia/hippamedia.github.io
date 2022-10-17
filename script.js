aafd30752945c2a76510d536be618c61748e6422
hippamedia <j.hyytiainen@gmail.com>
2022-10-17 14:36:17 +0300

    push

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ $(".btn").on("click", function(){
   $(".btn").toggleClass("close-btn");
   $(".menu").toggleClass("open-menu");
 });
+
+   $(window).scroll(function() {
+
+    if ($(this).scrollTop()>100)
+     {
+        $('.ball').fadeOut();
+     }
+    else
+     {
+      $('.ball').fadeIn();
+     }
+ });
