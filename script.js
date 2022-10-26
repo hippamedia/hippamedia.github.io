cd307dc6c4602aca38123e58c252a9df8d5dca05
hippamedia <j.hyytiainen@gmail.com>
2022-10-26 23:09:26 +0300

    menu

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-$( '.a' ).on("click", function(){
-  $('#menuToggle').click();
+$(document).ready(function(){
+  $('.menu-tab').click(function(){
+    $('.menu-hide').toggleClass('show');
+    $('.menu-tab').toggleClass('active');
+  });
+  $('a').click(function(){
+    $('.menu-hide').removeClass('show');
+    $('.menu-tab').removeClass('active');
+  });
 });
+
    $(window).scroll(function() {
 
     if ($(this).scrollTop()>100)
