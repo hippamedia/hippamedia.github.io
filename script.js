035b6a4761f5785df8b95533a1b7a145dd33909e
hippamedia <j.hyytiainen@gmail.com>
2023-03-16 22:00:34 +0200

    wave

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
         $(window).scroll(function(event) {
             let scroll = $(this).scrollTop();
             let opacity = 1 - (scroll / 1000);
-            if (opacity >= 0.1) {
+            if (opacity >= 1) {
                 $('.taustavideo video').css('opacity', opacity);
             }
         });
